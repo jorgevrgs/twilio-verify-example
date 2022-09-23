50fafc87bcbebd8103edd4fd100553ffff6b4ae2
Jorge Vargas <jorgevargasla@gmail.com>
2022-09-23 07:50:11 -0500

    feat: registration form skeleton

--- packages/ui/src/features/register/components/RegisterForm.vue
+++ packages/ui/src/features/register/components/RegisterForm.vue
@@ -1,49 +1,98 @@
 <template>
-  <form class="flex flex-col gap-4 mt-8">
+  <form @submit.prevent="onSubmit" class="flex flex-col gap-4 mt-8">
+    <MazInput
+      type="text"
+      id="username"
+      label="Username"
+      v-model="formData.username"
+      aria-label="Username"
+      autocomplete="username"
+      required
+    />
+
+    <MazInput
+      type="password"
+      v-model="formData.password"
+      id="password"
+      label="Password"
+      aria-label="Password"
+      autocomplete="password"
+      required
+    />
+
+    <MazPhoneNumberInput
+      id="phoneNumber"
+      label="Phone Number"
+      show-code-on-list
+      v-model="formData.phoneNumber"
+      required
+      :default-country-code="defaultCountryCode"
+      @update="onPhoneNumberUpdate"
+      :success="phoneNumberDetails?.isValid"
+    />
+
     <label class="flex flex-col">
-      <span>Username</span>
-      <input
-        class="form-input"
-        type="text"
-        v-model="formData.username"
-        aria-label="Username"
-        autocomplete="username"
+      <span>Enable MFA</span>
+      <MazSwitch
+        id="enableMFA"
+        color="primary"
+        v-model="formData.enableMFA"
+        required
       />
     </label>
 
-    <label class="flex flex-col">
-      <span>Password</span>
-      <input
-        class="form-input"
-        type="password"
-        v-model="formData.password"
-        aria-label="Password"
-        autocomplete="password"
-      />
-    </label>
-
-    <label class="flex flex-col">
-      <span>Phone Number</span>
-      <input
-        class="form-input"
-        type="tel"
-        v-model="formData.phoneNumber"
-        placeholder="573001234567"
-        aria-label="Phone Number"
-        autocomplete="tel"
-      />
-    </label>
-
-    <ToggleCheckbox v-model="formData.enableMFA" label="Enable MFA" />
+    <MazBtn type="submit" color="primary" :disabled="!isValid" class="self-end"
+      >Submit</MazBtn
+    >
   </form>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import type { CountryCode } from 'libphonenumber-js';
+import MazPhoneNumberInput from 'maz-ui/components/MazPhoneNumberInput';
+import MazBtn from 'maz-ui/components/MazBtn';
+import MazInput from 'maz-ui/components/MazInput';
+import MazSwitch from 'maz-ui/components/MazSwitch';
 import ToggleCheckbox from '../../../components/ToggleCheckbox.vue';
 
+interface RegisterFormProps {
+  defaultCountryCode: CountryCode;
+  isValid: boolean;
+  phoneNumberDetails?: {
+    isValid: false;
+    countryCode: string;
+    countryCallingCode: string;
+    nationalNumber: string;
+    type?: string;
+    formatInternational: string;
+    formatNational: string;
+    uri: string;
+    e164: string;
+  };
+  formData: {
+    username: string;
+    password: string;
+    phoneNumber: string;
+    enableMFA: boolean;
+  };
+}
+
 export default defineComponent({
-  data: () => ({
+  data: (): RegisterFormProps => ({
+    defaultCountryCode: 'CO',
+    isValid: false,
+    phoneNumberDetails: {
+      isValid: false,
+      countryCode: 'CO',
+      countryCallingCode: '',
+      nationalNumber: '',
+      type: '',
+      formatInternational: '',
+      formatNational: '',
+      uri: '',
+      e164: '',
+    },
     formData: {
       username: '',
       password: '',
@@ -51,6 +100,31 @@ export default defineComponent({
       enableMFA: false,
     },
   }),
-  components: { ToggleCheckbox },
+  components: {
+    ToggleCheckbox,
+    MazPhoneNumberInput,
+    MazSwitch,
+    MazInput,
+    MazBtn,
+  },
+  methods: {
+    onSubmit() {
+      console.log(this.formData);
+    },
+    onPhoneNumberUpdate(e: RegisterFormProps['phoneNumberDetails']) {
+      this.phoneNumberDetails = e;
+    },
+  },
+  watch: {
+    formData: {
+      handler() {
+        this.isValid =
+          Boolean(this.phoneNumberDetails?.isValid) &&
+          this.formData.username.length > 0 &&
+          this.formData.password.length > 0;
+      },
+      deep: true,
+    },
+  },
 });
 </script>
