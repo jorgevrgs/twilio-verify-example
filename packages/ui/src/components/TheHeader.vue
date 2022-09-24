64dfb8290f782d4be06ecf33c1e9cc208faf1a30
Jorge Vargas <jorgevargasla@gmail.com>
2022-09-24 17:30:48 -0500

    feat: add dynamic links

--- packages/ui/src/components/TheHeader.vue
+++ packages/ui/src/components/TheHeader.vue
@@ -2,35 +2,51 @@
   <header class="flex justify-center gap-8">
     <RouterLink
       class="w-16 text-center py-2 hover:font-bold"
-      :class="isActive('Home') ? 'underline underline-offset-8' : ''"
-      :to="{ name: 'Home' }"
-      >Home</RouterLink
+      :class="isActive(link.to) ? 'underline underline-offset-8' : ''"
+      v-for="link in links"
+      :key="link.to"
+      :to="{ name: link.to }"
     >
-    <RouterLink
-      class="w-16 text-center py-2 hover:font-bold"
-      :class="isActive('Register') ? 'underline underline-offset-8' : ''"
-      :to="{ name: 'Register' }"
-      >Register</RouterLink
-    >
-    <RouterLink
-      class="w-16 text-center py-2 hover:font-bold"
-      :class="isActive('Login') ? 'underline underline-offset-8' : ''"
-      :to="{ name: 'Login' }"
-      >Login</RouterLink
-    >
-    <RouterLink
-      class="w-16 text-center py-2 hover:font-bold"
-      :class="isActive('Profile') ? 'underline underline-offset-8' : ''"
-      :to="{ name: 'Profile' }"
-      >Profile</RouterLink
-    >
+      {{ link.name }}
+    </RouterLink>
   </header>
 </template>
 
 <script setup lang="ts">
+import { reactive } from 'vue';
 import { useRoute } from 'vue-router';
+import { useAuthStore } from '../features/auth/stores/register.store';
 
 const route = useRoute();
+const authStore = useAuthStore();
+
+const links = reactive<Array<{ name: string; to: string; isActive?: boolean }>>(
+  [
+    {
+      name: 'Home',
+      to: 'Home',
+    },
+  ]
+);
+
+if (authStore.isAuthenticated) {
+  links.push({
+    name: 'Profile',
+    to: 'Profile',
+  });
+} else {
+  links.push(
+    {
+      name: 'Login',
+      to: 'Login',
+    },
+    {
+      name: 'Register',
+      to: 'Register',
+    }
+  );
+}
+
 const isActive = (name: string) => route.name === name;
 </script>
 
