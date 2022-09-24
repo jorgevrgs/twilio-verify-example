8e1db94c7e5a0753bdf70632734751cff5aac986
Jorge Vargas <jorgevargasla@gmail.com>
2022-09-24 08:47:15 -0500

    feat: add log in instead link

--- packages/ui/src/pages/RegisterPage.vue
+++ packages/ui/src/pages/RegisterPage.vue
@@ -1,11 +1,19 @@
 <template>
   <h1>Register</h1>
+
   <RegisterForm />
+
+  <hr class="my-8" />
+
+  <p class="text-center">
+    Already have an account?
+    <RouterLink :to="{ name: 'Login' }">Login</RouterLink>
+  </p>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import RegisterForm from '../features/register/components/RegisterForm.vue';
+import RegisterForm from '../features/auth/components/RegisterForm.vue';
 
 export default defineComponent({
   setup() {
