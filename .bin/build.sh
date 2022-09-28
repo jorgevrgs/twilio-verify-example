1f4226b64f369a6a7b1751d472292197f2deb2ed
Jorge Vargas <jorgevargasla@gmail.com>
2022-09-28 17:08:36 -0500

    fix: location proxy

--- .bin/build.sh
+++ .bin/build.sh
@@ -1,15 +1,15 @@
 set -x
 
-cd packages/api
-set -a
-. ./.env.docker
-set +a
-docker build --no-cache -t twilio/api:latest -f Dockerfile .
-cd -
+# cd packages/api
+# set -a
+# . ./.env.docker
+# set +a
+# docker build --no-cache -t twilio/api:latest -f Dockerfile .
+# cd -
 
 cd packages/ui
 set -a
 . ./.env.docker
 set +a
-docker build --no-cache -t twilio/ui:latest -f Dockerfile --build-args VITE_BACKEND_URL=http://twilio_api:1337 .
+docker build --no-cache -t twilio/ui:latest -f Dockerfile --build-arg VITE_BACKEND_URL=http://twilio_api:1337 .
 cd -
