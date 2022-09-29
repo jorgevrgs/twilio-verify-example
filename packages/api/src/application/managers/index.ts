cb10b1a30709aced1a4035f865b855122e25b152
Jorge Vargas <jorgevargasla@gmail.com>
2022-09-28 23:36:29 -0500

    wip: add users service objects

---
+++ packages/api/src/application/managers/index.ts
@@ -0,0 +1,3 @@
+export * from './challenge-manager';
+export * from './device-manager';
+export * from './verify-manager';
--- packages/api/src/application/services/index.ts
+++ packages/api/src/application/services/index.ts
@@ -1,3 +1,3 @@
-export * from './challenge-manager';
-export * from './device-manager';
-export * from './verify-manager';
+export * from './auth.service';
+export * from './helpers.service';
+export * from './users.service';
